dc021cb425cb92660c979a243554e410951b8897
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-11 00:21:40 +0300

    adding-project

---
+++ app/add-project/page.tsx
@@ -0,0 +1,4 @@
+import AddingProjectFunction from "@/components/adding-project-component"
+export default function AddingProject() {
+      return <AddingProjectFunction/>
+}
---
+++ components/adding-project-component.tsx
@@ -0,0 +1,18 @@
+import SideNavBar from "./client/side-nav"
+export default function AddingProjectFunction() {
+      return (<>
+            <SideNavBar />
+            <div className="adding-project-function ml-[400px] pt-[80px]">
+            <div className="adding-task-heading">
+                        <h1 className="font-bold text-[20px]">Adding Project</h1>
+                        <h1 className="text-[#5f5f5f]">add new project </h1>
+                  </div>
+
+
+                  <div className="adding-project-input-section">
+                        
+                  </div>
+
+            </div>
+      </>)
+}
