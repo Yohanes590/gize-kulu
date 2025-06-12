7cdc451e7e8b1f4b036cc8017c405e5a561540b3
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-12 17:53:03 +0300

    about-compo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import LandingPage from "@/components/home/landing-page";
 import Navigation from "@/components/home/navigation/navigation";
+import AboutPage from "@/components/home/about-component";
 export default function Home() {
   return (<>
     <Navigation/>
-    <LandingPage/>
+    <LandingPage />
+    <AboutPage/>
   </>);
 }
--- components/home/about-component.tsx
+++ components/home/about-component.tsx
@@ -1,7 +1,14 @@
 export default function AboutPage() {
       return <>
       
-            
+            <div className="about-container text-center h-screen">
+
+                  <div className="about-header">
+                        <h1 className="font-bold text-[35px] text-[#5e5e5e]">Main Features </h1>
+                        <h3 className="text-[#858585] text-[18px]">Everything you need to simplify your projects and boost productivity</h3>
+                  </div>
+
+            </div>
       
       </>
 }
