0bf269b0d3f08e3faec4120751dde7dc2014156c
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-17 14:53:15 +0300

    make shadow

--- components/home/about-landing-page.tsx
+++ components/home/about-landing-page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image"
 export default function PicAbout() {
       return <>
-            <div className="h-screen flex justify-center items-center">
+            <div className="h-screen flex justify-center items-center ">
                   
-                  <Image src="/about-pic-cover.png" width={1000} height={100} alt="about picture dashboard" className="w-[80%]"/>
+                  <Image src="/about-pic-cover.png" width={1000} height={100} alt="about picture dashboard" className="w-[80%]
+                  shadow-[0_0_5px_#e7e7e7] rounded-[10px]
+                  "/>
 
            </div>
       </>
