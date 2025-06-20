03b7173682d324f2b04c35f1c13590e1e175d67a
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-20 21:54:15 +0300

    basic otp

---
+++ app/API/Authentication/otp/route.ts
@@ -0,0 +1,9 @@
+export async function POST(userRequest: Request) {
+      const userRequestBody = await userRequest.json()
+      if (userRequestBody.length > 1) {
+            return Response.json({message:"invalid Key" ,status:400})
+      } else {
+            const UserToken = userRequestBody.user_toke
+            return Response.json({user_token:UserToken})
+      }
+}
