ef674c8c87aec554f99486839bc13e178d5d7f47
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-20 22:47:38 +0300

    backend basic setup

--- app/API/Authentication/otp/route.ts
+++ app/API/Authentication/otp/route.ts
@@ -3,55 +3,64 @@ import prisma from "@/lib/prisma"
 import nodemailer from 'nodemailer'
 export async function POST(userRequest: Request) {
       const userRequestBody = await userRequest.json()
-      if (userRequestBody.length > 1) {
+      if (userRequestBody.length > 2) {
             return Response.json({message:"invalid Key" ,status:400})
       } else {
             const UserToken = userRequestBody.user_token
+            const userOtp = userRequestBody.user_otp
             const verifyKey=process.env.ACCESS_TOKEN
             if (!verifyKey) {
                   return Response.json({message:"invalid secret key" ,status:500})
             }
             try {
                   const verify = jwt.verify(UserToken, verifyKey) as JwtPayload
-                  const FetchEmail = verify.user_info.user_email
-
+                  const FetchEmail = verify.userInfo.user_email
                   let OTP;
-                  const transport = nodemailer.createTransport({
-                        service: "Gmail",
-                        auth: {
-                              user: process.env.EMAIL_PASS,
-                              pass:process.env.GOOGE_SECRET
-                        }
-                  })
-                   OTP = Math.floor(100000 + Math.random() * 900000)
-                  const mailOption = {
-                        from: process.env.EMAIL_PASS,
-                        to: FetchEmail,
-                        subject: "Your Verification Key From GIZE KULU APP",
-                        html: `
-                        <h1>Verification Key From Gize Kulu Team. Don't share for any body ! </h1>
-                        <p>${OTP}</p>
-                        `
-                  }
+                  OTP = Math.floor(100000 + Math.random() * 900000)
                   setTimeout(() => {
                         OTP = null;
                   }, 500000);
-                  try {
-                        await transport.sendMail(mailOption)
-                        const GettingUser = await prisma.user.update({
-                              where: { user_email: FetchEmail },
-                              data: {
-                                    user_verify:true
+                  if (userOtp == "") {
+                        const transport = nodemailer.createTransport({
+                              service: "Gmail",
+                              auth: {
+                                    user: process.env.EMAIL_PASS,
+                                    pass:process.env.GOOGE_SECRET
                               }
                         })
-                        if (!GettingUser) {
-                              return Response.json({message:"invalid secrete key",status:400})   
+                        const mailOption = {
+                              from: process.env.EMAIL_PASS,
+                              to: FetchEmail,
+                              subject: "Your Verification Key From GIZE KULU APP",
+                              html: `
+                              <h1>Verification Key From Gize Kulu Team. Don't share for any body ! </h1>
+                              <p>${OTP}</p>
+                              `
                         }
-                        return Response.json(verify)
-                  } catch (error) {
-                        return Response.json({message:"something wen't error"})
+                  
+                        try {
+                              await transport.sendMail(mailOption)
+                              return Response.json({message:"send success fully",status:200})
+                        } catch (error) {
+                              return Response.json({message:"something wen't error",status:500})
+                        }
+      
+                  } else {
+                        if (OTP != userOtp) {
+                              return Response.json({message:"invalid otp"})
+                        } else {
+                              const GettingUser = await prisma.user.update({
+                                    where: { user_email: FetchEmail },
+                                    data: {
+                                          user_verify:true
+                                    }
+                              })
+                              if (!GettingUser) {
+                                    return Response.json({message:"invalid secrete key",status:400})   
+                              }
+                              return Response.json({message:"verified success fully",status:200})
+                        }
                   }
-
    
             } catch (error:any) {
                   return Response.json({message: error.message})   
