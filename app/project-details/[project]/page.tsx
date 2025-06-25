b8a9bccadd2bee939faadcbd51828237f18d4eda
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-25 13:18:24 +0300

    sync changes

--- app/project-details/[project]/page.tsx
+++ app/project-details/[project]/page.tsx
@@ -1,5 +1,4 @@
 "use client";
 export default function ClientProjectDetails({ project }: { project: string }) {
-  console.log("Rendering ClientProjectDetails with project:", project);
   return <p>project details: {project}</p>;
 }
