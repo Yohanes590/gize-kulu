19a1f1eee86f5b1e2fd3fe310d0a31d890bc4ed0
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-25 15:52:39 +0300

    sync changes

--- app/project-details/[project]/page.tsx
+++ app/project-details/[project]/page.tsx
@@ -8,7 +8,9 @@ export default function ClientProjectDetails() {
   const TaskArrays = [{
     task_name: "some task",
     priority:"complete",
-    start_date:"complete"
+    start_date: "20/02/2024",
+    due_date: "02/12/2025",
+    title_description:"some description"
   }]
 
   return <>
@@ -19,8 +21,22 @@ export default function ClientProjectDetails() {
         <p>Project Details / { decodeURI(project) }</p>
         </div>
 
-      
+      <div className="table-card mt-[100px]">
 
+        <div className="each-table-card w-[90%] cursor-pointer rounded-[10px] h-[200px] shadow-[0_0_5px_#747474]">
+
+          <div className="inner-part pl-[20px] pt-[20px]">
+          <h1 className=" text-[#252525] text-[20px] ">Title Name</h1>
+          <p className="pt-[10px]">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Fugit nostrum quisquam dolorum corporis et eveniet in ipsa rem ab nam,
+            sint, optio vitae, assumenda labore voluptas beatae itaque nemo hic.</p>
+          </div>
+    
+          
+        </div>
+
+
+      </div>
 
     </div>
   </>;
