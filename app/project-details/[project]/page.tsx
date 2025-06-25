e6cfef2f751d587c71396cf6cce934448eea4a90
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-25 15:25:05 +0300

    page sync changes sync

--- app/project-details/[project]/page.tsx
+++ app/project-details/[project]/page.tsx
@@ -4,6 +4,13 @@ import SideNavBar from "@/components/client/side-nav";
 export default function ClientProjectDetails() {
   const params = useParams();
   const project = params.project as string
+
+  const TaskArrays = [{
+    task_name: "some task",
+    priority:"complete",
+    start_date:"complete"
+  }]
+
   return <>
     <SideNavBar />
     <div className="project-details-container ml-[400px] pt-[150px]">
@@ -11,6 +18,8 @@ export default function ClientProjectDetails() {
       <div className="project-name text-[18px] text-[#747474]">
         <p>Project Details / { decodeURI(project) }</p>
         </div>
+
+      
 
 
     </div>
