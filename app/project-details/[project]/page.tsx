027dac3bff6a880374092d5b800eb079019c0229
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-25 14:18:19 +0300

    prams change

--- app/project-details/[project]/page.tsx
+++ app/project-details/[project]/page.tsx
@@ -1,10 +1,7 @@
 "use client";
-
 import { useParams } from "next/navigation";
-
 export default function ClientProjectDetails() {
   const params = useParams();
-  const project = params.project 
-
-  return <p>Project details: {project}</p>;
+  const project = params.project as string
+  return <p>Project details: {decodeURI(project)}</p>;
 }
