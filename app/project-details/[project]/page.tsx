4096376df08b7840d0eb3ef338943c3a695c3d74
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-25 14:29:32 +0300

    adding side bar

--- app/project-details/[project]/page.tsx
+++ app/project-details/[project]/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { useParams } from "next/navigation";
+import SideNavBar from "@/components/client/side-nav";
 export default function ClientProjectDetails() {
   const params = useParams();
   const project = params.project as string
-  return <p>Project details: {decodeURI(project)}</p>;
+  return <>
+  <SideNavBar/>
+  </>;
 }
