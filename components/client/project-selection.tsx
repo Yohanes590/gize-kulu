478c39c08f79e6e61cfe6a7068b719f38deaddfe
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-26 14:03:23 +0300

    gettingProjectName

--- components/client/project-selection.tsx
+++ components/client/project-selection.tsx
@@ -12,9 +12,31 @@ type ProjectTypeDefine = {
   OnProjectChange:(ProjectSelectionType:string)=>void
 }
 
-import { useEffect } from "react"
+import { useEffect , useState } from "react"
+import Cookies from "js-cookie"
+export default function ProjectSelection({ ProjectSelectionType, OnProjectChange }: ProjectTypeDefine) {
 
-export default function ProjectSelection({ ProjectSelectionType, OnProjectChange }:ProjectTypeDefine) {
+  type ProjectNameType = {
+    project_name:string
+  }
+  const [ projectArray , setProjectArray ] = useState<ProjectNameType[]>()
+  useEffect(() => {
+    const gettingProjectsFunction = async () => {
+      const gettingToken = Cookies.get("access-token")
+      const sendCookie = await fetch("/API/cli/project/show-projects", {
+        method: "post",
+        headers: {
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+          user_token:gettingToken,
+        })
+      })
+      const serverResponse = await sendCookie.json()
+      setProjectArray(serverResponse)
+    }
+    gettingProjectsFunction()
+  },[])
       return (<>
             <Select value={ProjectSelectionType} onValueChange={OnProjectChange}>
           <SelectTrigger className="w-[650px]" style={{
@@ -22,8 +44,13 @@ export default function ProjectSelection({ ProjectSelectionType, OnProjectChange
   }}>
     <SelectValue placeholder="Select Project" />
   </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="My Test Project">MY TEST PROJECT</SelectItem>
+          <SelectContent>
+            {projectArray?.length === 0 ? (
+    <SelectItem value="">No Project Listed</SelectItem>
+            ) : (
+                projectArray?.map((projectItems) =>
+                <SelectItem key={projectItems.project_name} value={projectItems.project_name}>{projectItems.project_name}</SelectItem>)
+            )}
   </SelectContent>
 </Select>
       </>)
