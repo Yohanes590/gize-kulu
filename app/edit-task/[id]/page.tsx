010eb0c1c00e5be08c626cfa8cff7ca835e56bed
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-29 22:41:48 +0300

    use pramas

--- app/edit-task/[id]/page.tsx
+++ app/edit-task/[id]/page.tsx
@@ -1,5 +1,9 @@
-export default function EditTaskInfo (){
+"use client"
+import { useParams } from "next/navigation"
+export default function EditTaskInfo() {
+      const params = useParams()
+      const fetchPrams = params.id as string
       return <>
-        editing task
+        editing task {fetchPrams}
       </>
 }
