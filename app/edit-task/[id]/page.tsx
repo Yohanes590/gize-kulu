f200f6b4ad5d5fd7506cf47712523c570ff5156e
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-29 22:46:08 +0300

    input button

--- app/edit-task/[id]/page.tsx
+++ app/edit-task/[id]/page.tsx
@@ -4,6 +4,11 @@ export default function EditTaskInfo() {
       const params = useParams()
       const fetchPrams = params.id as string
       return <>
-        editing task {fetchPrams}
+            <div className="editing-task-part ml-[400px] mt-[150px]">
+                  <div className="editing-task-container">
+                        <input type="text" placeholder="Task name" className="w-[350px] h-[40px] pl-[20px] outline-1 outline-blue-500 bg-[#f1f1f1]" />
+                        <input type="text" placeholder="Task name" className="w-[350px] h-[40px] pl-[20px] outline-1 outline-blue-500 bg-[#f1f1f1]" />
+                         </div>
+        </div>
       </>
 }
