cb396167da7729e6625259849201d1023fac1c2e
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-30 21:43:06 +0300

    task frontend

--- app/edit-task/[id]/page.tsx
+++ app/edit-task/[id]/page.tsx
@@ -6,9 +6,27 @@ export default function EditTaskInfo() {
       return <>
             <div className="editing-task-part ml-[400px] mt-[150px]">
                   <div className="editing-task-container">
-                        <input type="text" placeholder="Task name" className="w-[350px] h-[40px] pl-[20px] outline-1 outline-blue-500 bg-[#f1f1f1]" />
-                        <input type="text" placeholder="Task name" className="w-[350px] h-[40px] pl-[20px] outline-1 outline-blue-500 bg-[#f1f1f1]" />
-                         </div>
+                        <div className="each-input-section mb-[20px] flex flex-wrap gap-5">
+                        <input type="text" placeholder="Task title" className="w-[550px] h-[40px] pl-[20px] outline-1 outline-blue-500 bg-[#f1f1f1]" />
+                        <select className=" w-[550px] h-[40px] pl-[20px] outline-1 outline-blue-500 bg-[#f1f1f1]">
+                              <option value="low">Select Priority</option>
+                              <option value="low">Low</option>
+                              <option value="medium">Medium</option>
+                              <option value="high">High</option>
+                        </select>
+                        </div>
+
+                        <div className="each-input-section flex flex-wrap gap-5">
+                              <div className="label">
+                                    <p>Start date</p>
+                                     <input type="date"  className=" w-[550px] h-[40px] pl-[20px] outline-1 outline-blue-500 bg-[#f1f1f1]"/>
+                              </div>
+                                                       <div className="label">
+                                    <p>Start date</p>
+                                     <input type="date"  className=" w-[550px] h-[40px] pl-[20px] outline-1 outline-blue-500 bg-[#f1f1f1]"/>
+                              </div>
+                        </div>
+                  </div>
         </div>
       </>
 }
