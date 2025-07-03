620092b1d569bf8d908062689bbfe161bc107e92
Yohanes Mulugeta <jplussince34@gmail.com>
2025-07-03 13:18:13 +0300

    Appending footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import LandingPage from "@/components/home/landing-page";
 import Navigation from "@/components/home/navigation/navigation";
 import AboutPage from "@/components/home/about-component";
 import PicAbout from "@/components/home/about-landing-page";
+import FooterFunction from "@/components/home/footer";
 export default function Home() {
   return (<>
     <Navigation/>
     <LandingPage />
     <AboutPage />
-    <PicAbout/>
+    <PicAbout />
+    <FooterFunction/>
   </>);
 }
