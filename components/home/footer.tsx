54ddd7b67beb4d9a42617093125a5489c0ef6eec
Yohanes Mulugeta <jplussince34@gmail.com>
2025-07-04 19:01:41 +0300

    sync changes

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -3,7 +3,10 @@ export default function FooterFunction() {
             <div className="footer-container text-white h-[200px] flex justify-center items-center flex-wrap w-[100%] bg-[#000f25]">
 
                   <div className="each-flex-items-footer">
-                        <span>Home</span>
+                        <span>Home</span><br/>
+                        <span>About</span><br/>
+                        <span>Sign up</span><br/>
+                        <span>Sign in</span><br/>
             </div>
 
             </div>
