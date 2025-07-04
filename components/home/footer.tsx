d1abc903510151220acdb538bc5053d7292f46cb
Yohanes Mulugeta <jplussince34@gmail.com>
2025-07-04 19:00:45 +0300

    sync commit

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -1,6 +1,6 @@
 export default function FooterFunction() {
       return <>
-            <div className="footer-container text-white h-[400px] flex justify-center items-center flex-wrap w-[100%] bg-[#000f25]">
+            <div className="footer-container text-white h-[200px] flex justify-center items-center flex-wrap w-[100%] bg-[#000f25]">
 
                   <div className="each-flex-items-footer">
                         <span>Home</span>
