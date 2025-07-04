dcbf45ca3903bc9d01cb9a2d0a27d0986a652595
Yohanes Mulugeta <jplussince34@gmail.com>
2025-07-04 19:06:46 +0300

    sync changes

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -1,13 +1,20 @@
 export default function FooterFunction() {
       return <>
-            <div className="footer-container text-white h-[200px] flex justify-center items-center flex-wrap w-[100%] bg-[#000f25]">
+            <div className="footer-container text-white h-[200px] flex justify-evenly items-center flex-wrap w-[100%] bg-[#000f25]">
 
                   <div className="each-flex-items-footer">
                         <span>Home</span><br/>
                         <span>About</span><br/>
                         <span>Sign up</span><br/>
-                        <span>Sign in</span><br/>
-            </div>
+                        <span>Login</span><br/>
+                  </div>
+                  
+                  <div className="each-flex-items-footer">
+                        <p>This web app is powered by Yohanes Mulugeta</p>
+                  </div>
+  
+                  
+
 
             </div>
       </>
