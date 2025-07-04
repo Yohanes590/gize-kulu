e5e8e9bab91185175ca27da530ee1cf32598e831
Yohanes Mulugeta <jplussince34@gmail.com>
2025-07-04 18:59:42 +0300

    footer-update

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -1,7 +1,10 @@
 export default function FooterFunction() {
       return <>
-            <div className="footer-container h-[400px] w-[100%] bg-[#000f25]">
+            <div className="footer-container text-white h-[400px] flex justify-center items-center flex-wrap w-[100%] bg-[#000f25]">
 
+                  <div className="each-flex-items-footer">
+                        <span>Home</span>
+            </div>
 
             </div>
       </>
