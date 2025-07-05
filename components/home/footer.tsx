a4781ea115677e63fc43e48bdff5b2299cc93db2
Yohanes Mulugeta <jplussince34@gmail.com>
2025-07-05 21:47:26 +0300

    fix type error

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -3,7 +3,7 @@ export default function FooterFunction() {
             <div className="footer-container text-center text-white h-[100px] flex justify-evenly items-center flex-wrap w-[100%] bg-[#000f25]">
                   <footer className="each-flex-items-footer">
                         <p>This web app is powered by Yohanes Mulugeta</p>
-                        <p>&copy;Copy right { new Date().getFullYear() } </p>
+                        <p>&copy;Copy right {new Date().getFullYear()}</p>
                   </footer>
             </div>
       </>
