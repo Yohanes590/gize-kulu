57cad2c0ffc3bb13e01a5969cb62b4bcc6c2f5f9
Yohanes Mulugeta <jplussince34@gmail.com>
2025-07-05 21:45:55 +0300

    footer year

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -1,21 +1,10 @@
 export default function FooterFunction() {
       return <>
-            <div className="footer-container text-white h-[200px] flex justify-evenly items-center flex-wrap w-[100%] bg-[#000f25]">
-
-                  <div className="each-flex-items-footer">
-                        <span>Home</span><br/>
-                        <span>About</span><br/>
-                        <span>Sign up</span><br/>
-                        <span>Login</span><br/>
-                  </div>
-                  
-                  <div className="each-flex-items-footer">
+            <div className="footer-container text-center text-white h-[50px] flex justify-evenly items-center flex-wrap w-[100%] bg-[#000f25]">
+                  <footer className="each-flex-items-footer">
                         <p>This web app is powered by Yohanes Mulugeta</p>
-                  </div>
-  
-                  
-
-
+                        <p>&copy;Copy right { new Date().getFullYear() }</p>
+                  </footer>
             </div>
       </>
 }
